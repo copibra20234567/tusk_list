bd3ecb43dc5e77f80294ee06892ac2458d2d5090
copibra20234567 <130291138+copibra20234567@users.noreply.github.com>
2024-11-13 12:28:43 +0200

    ю

--- app/urls.py
+++ app/urls.py
@@ -1,15 +1,17 @@
-from django.urls import path
+from django.urls import path, include
 from app.views import TaskListView, TaskDetailView , TaskCreateView
 
+app_name = "app"
+
 urlpatterns=[
-    path("tasklist/",TaskListView.as_view(), name='tasklist'),
+    path("app:tasklist",TaskListView.as_view(), name='tasklist'),
 
     path("<int:pk>/",TaskDetailView.as_view(), name='task-detail'),
 
-    path("task-create>/",TaskCreateView.as_view(), name='task-create')
+    path("task-create>/",TaskCreateView.as_view(), name='task-create'),
 
+    path('', include('app.urls', namespace='app'))
 
 ]
 
 
-app_name = "app"
